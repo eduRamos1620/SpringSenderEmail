8d96e613f1f8789d17f138bca9eac9eea9f2d3a9
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-08-15 17:57:12 -0600

    Proyecto para el consumo de api para envio de correos

---
+++ src/main/java/com/ramos/Correo/CorreoApplication.java
@@ -0,0 +1,13 @@
+package com.ramos.Correo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreoApplication.class, args);
+	}
+
+}
