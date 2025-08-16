c4883e64202278192e8e9cadb1cb8dbf90dced07
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-08-15 19:27:40 -0600

    Corrigiendo temas para el envio

--- src/main/java/com/ramos/Correo/config/MailConfiguration.java
+++ src/main/java/com/ramos/Correo/config/MailConfiguration.java
@@ -26,7 +26,7 @@ public class MailConfiguration {
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls", "true");
+        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
 
         return mailSender;
